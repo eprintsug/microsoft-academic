878d98d302b091fb95f8091840d597c0756b3fc7
mpbraendle <marbra@idmacmb.uzh.ch>
2017-09-07 15:40:44 +0200

    Script and MSAcademic citation import plug-in

--- cfg/cfg.d/z_ms_knowledge_api.pl
+++ cfg/cfg.d/z_ms_knowledge_api.pl
@@ -5,36 +5,36 @@
 #
 ###############################################################################
 
-$c->{msapi} = {};
+$c->{msacademic} = {};
 
 #
 # The MS Academic Knowledge API developer key
 #
-$c->{msapi}->{apikey} = "insert_your_key_here";
+$c->{msacademic}->{apikey} = "insert_key_here";
 #
 # The base URL for the Evaluate REST Endpoint
 #
-$c->{msapi}->{uri} = URI->new( 'https://api.projectoxford.ai/academic/v1.0/evaluate' );
+$c->{msacademic}->{uri} = URI->new( 'https://westus.api.cognitive.microsoft.com/academic/v1.0/evaluate' );
 #
 # The fields that shall be returned by the MS Academic Knowledge API
 #
-$c->{msapi}->{msapi_fields} = "Id,Ti,Y,D,CC,ECC,AA.AuN,AA.AuId,AA.AfN,AA.AfId,F.FN,F.FId,J.JN,J.JId,C.CN,C.CId,RId,E";
+$c->{msacademic}->{msacademic_fields} = "Id,Ti,Y,D,CC,ECC,AA.AuN,AA.AuId,AA.AfN,AA.AfId,F.FN,F.FId,J.JN,J.JId,C.CN,C.CId,RId,E";
 #
 # The number of records that shall be returned by the MS Academic Knowledge API
 #
-$c->{msapi}->{answer_count} = 10;
+$c->{msacademic}->{answer_count} = 10;
 #
 # The crawl delay in seconds between two queries
 #
-$c->{msapi}->{crawl_delay} = 1;
+$c->{msacademic}->{crawl_delay} = 1;
 #
 # The number of retries if there is no answer after 60 seconds
 #
-$c->{msapi}->{crawl_retry} = 3;
+$c->{msacademic}->{crawl_retry} = 3;
 #
 # The EPrints fields that shall be output together with the MS Academic Knowledge API results
 #
-$c->{msapi}->{eprint_fields} = [
+$c->{msacademic}->{eprint_fields} = [
   'eprintid',
   'title',
   'type',
@@ -54,5 +54,5 @@ $c->{msapi}->{eprint_fields} = [
 #
 # The affiliation id of the institution of interest.
 #
-$c->{msapi}->{affiliation_id} = 202697423;
+$c->{msacademic}->{affiliation_id} = 202697423;
 
